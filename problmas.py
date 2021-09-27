791f3448e634e7f275ff6585e62c231c57103dad
NormanBrandon <saldananormanbrandon1iv18@gmail.com>
2021-09-27 15:52:58 -0500

    Problemas

--- problmas.py
+++ problmas.py
@@ -11,8 +11,40 @@ Problema ={
 	'Diana':['Juan'],
 }
 Problema ={
-	
-
-
-
+'Luis':[],
+	'Maria':[]
+}
+Problema ={
+	'Laura':['Jorge','Juan','Nancy'],
+	'Jose':['Diana','Fernanda','Carlos','Diana','Axel'],
+	'Liliana':['Axel','Fernanda'],
+	'Juan':['Carlos','Laura','Jorge'],
+	'Jorge':['Laura','Juan','Axel'],
+	'Diana':['Jose','Nancy','Jose'],
+	'Carlos':['Juan','Jose','Fernanda'],
+	'Nancy':['Diana','Laura'],
+	'Axel':['Liliana','Jorge','Jose'],
+	'Fernanda':['Jose','Liliana','Carlos']
+}
+Problema ={
+	'Laura':['Jorge','Ana'],
+	'Jose':['Diana','Gabriela'],
+	'Liliana':['Axel','David','Gabriela'],
+	'Juan':['Carlos','David','Gabriela'],
+	'Jorge':['Laura','Diana'],
+	'Diana':['Jose','Jorge','Nancy'],
+	'Carlos':['Juan','Gabriela'],
+	'Nancy':['Arturo','Diana','Angel'],
+	'Axel':['Liliana','Gabriela'],
+	'Fernanda':['Diego','Gabriela'],
+	'Oscar':['David','Maria','Gabriela'],
+	'Maria':['Gabriela','Oscar','Christian'],
+	'Angel':['Christian','Nancy','Arturo'],
+	'Arturo':['Nancy','Angel','Diego'],
+	'Ana':['Karen','Laura','Gabriela'],
+	'Diego':['Fernanda','Arturo','Karen'],
+	'David':['Oscar','Liliana','Juan'],
+	'Gabriela':['Maria','Jose','Carlos','Axel','Fernanda','Ana','Oscar','Liliana','Juan'],
+	'Christian':['Angel','Maria'],
+	'Karen':['Ana','Diego']
 }
