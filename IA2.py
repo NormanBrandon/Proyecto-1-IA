a8e71c5e872a5506072e9dc50ac370bb35f9fe7b
NormanBrandon <saldananormanbrandon1iv18@gmail.com>
2021-09-27 14:53:38 -0500

    Comentarios IA2

--- IA2.py
+++ IA2.py
@@ -1,27 +1,24 @@
 from queue import Queue
 
-examenes=[1,2,3]
-amigos =[]
-alumnos_visitados =[]
-Asignacion = {}
-
-Alumnos = {
+examenes=[1,2,3]#lista de exámenes
+Asignacion = {}#inicialización de diccionario de asignación vacio
+Problema = {
 	'Juan':['Jorge','Diana'],
 	'Jorge':['Juan'],
 	'Diana':['Juan'],
 }
+#Problema.keys obtiene todos los alumnos del problema
+for node in Problema.keys(): #inicializamos el diccionario de asignaciones con los alumnos, asignando 0 a todos (no se ha asignado examen)
+	Asignacion[node] = 0 # Asigna 0 a cada alumno, donde node el es alumno, rellena el diccionario
 
-for node in Alumnos.keys(): #inicializamos el diccionario de asignaciones con los alumnos, asignando 0 a todos (no se ha asignado examen)
-	Asignacion[node] = 0
-print(Asignacion)
+print(Asignacion)#imprime ['Juan':1,'Jorge':0,'Diana':0], el diccionario tiene el formato adecuado en este punto
 	 
-
-for i in examenes:
-	Asignacion['Juan'] = i
-	for j in examenes:
-		Asignacion['Jorge'] = j	
-		for k in examenes:
-			Asignacion['Diana'] = k
-			print(Asignacion)
+for i in examenes: #iteramos 3 veces; i itera en la lista [1 2 3]
+	Asignacion['Juan'] = i #asignamos el valor de i a Juan en el diccionario de asignacion
+	for j in examenes: #iteramos 3 veces; j itera en la lista [1 2 3]
+		Asignacion['Jorge'] = j	#asignamos el valor de j a Juan en el diccionario de asignacion
+		for k in examenes: # iteramos 3 veces; k itera en la lista [1 2 3]
+			Asignacion['Diana'] = k # asignamos el valor de k a Juan en el diccionario de asignacion
+			print(Asignacion) # imprime la asignación Final	
 		print()
 		
